8d3062fcc2323fa164c329e13b9be8138ab5d943
Afeez Bolaji <afeezbolajiola@gmail.com>
2024-08-26 19:23:18 +0200

    Setting up the python envirenoment

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 import tornado.web
 import tornado.ioloop
+
+
+if __name__ == "__main__":
+    app = tornado.web.Aplication([
+        (r"/", basicRequestHandler)
+    ])
+    
+    port = 3000
+    app.listen(port)
+    print(f"App is listening on port: {port} 🔥🔥🔥")

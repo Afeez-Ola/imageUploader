584812b599768c72ec84fc06febd7c608b8f2afa
Afeez Bolaji <afeezbolajiola@gmail.com>
2024-08-26 19:38:39 +0200

    .

--- main.py
+++ main.py
@@ -2,18 +2,18 @@ import tornado.web
 import tornado.ioloop
 
 
-class basicRequestHandler(tornado.web.RequestHandler):
+class uploadHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("We are live baby!")
     
 
-
 if __name__ == "__main__":
     app = tornado.web.Application([
-        (r"/", basicRequestHandler)
+        (r"/", uploadHandler),
+        (r"/img/(.*)", tornado.web.StaticFileHandler, {"path": "img"})
     ])
     
     port = 3000
     app.listen(port)
     print(f"App is listening on port: {port} 🔥🔥🔥")
-    tornado.ioloop.IOLoop.current().start()
+    tornado.ioloop.IOLoop.instance().start()
